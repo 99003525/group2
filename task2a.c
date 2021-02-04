8dba0fe7ba9817543f1f24155f63bd39a3271697
stepin104282 <69046442+stepin104282@users.noreply.github.com>
2021-02-04 15:19:33 +0530

    hey

--- task2a.c
+++ task2a.c
@@ -6,10 +6,6 @@ int main()
     printf("I am Sneha \n");
     printf("I am Spoorthy \n");
     printf("I am Sahil \n");
-<<<<<<< HEAD
     printf("I am Sri Naga Anjaneya \n");
-=======
-    printf("I am Malavika \n");
->>>>>>> d84f64cc016d076a8e8c70a2a2cb28c2211a2b28
     return 0;
 }
