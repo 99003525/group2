c37529497a7aa9804b81092578bbc29e238e895c
stepin105186 <spoorthyprem07@gmail.com>
2021-02-04 14:57:26 +0530

    team description

--- task2a.c
+++ task2a.c
@@ -2,7 +2,8 @@
 int main()
 {
     printf("Hello team! \n");
-    printf("I am Pranshi !");
-    printf("I am Sneha ");
+    printf("I am Pranshi !\n");
+    printf("I am Sneha \n");
+    printf("I am Spoorthy \n");
     return 0;
 }
